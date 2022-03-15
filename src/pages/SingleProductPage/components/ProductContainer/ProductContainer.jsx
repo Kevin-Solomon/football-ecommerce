ccf99f89fd5e701c673a9d2a53e4864bd5eb6f1e
Kevin-Solomon <kevinsolomon404@gmail.com>
2022-03-15 16:14:52 +0530

    refactor : used find instead of filter mathod

--- src/pages/SingleProductPage/components/ProductContainer/ProductContainer.jsx
+++ src/pages/SingleProductPage/components/ProductContainer/ProductContainer.jsx
@@ -10,36 +10,35 @@ function ProductContainer() {
   useEffect(() => {
     async function getProducts() {
       const response = await axios.get('/api/products');
-
       setData(
-        ...response.data.products.filter(item => item.id === params.productId)
+        response.data.products.find(item => item.id === params.productId)
       );
     }
     getProducts();
   }, []);
-  console.log(data);
+
   return (
     <>
-      <section class="single-product-container">
-        <div class="product-img-container">
-          <img class="responsive-img" src={data.imgSrc} />
+      <section className="single-product-container">
+        <div className="product-img-container">
+          <img className="responsive-img" src={data.imgSrc} />
         </div>
-        <div class="product-detail-container">
-          <form class="product-form" action="">
+        <div className="product-detail-container">
+          <form className="product-form" action="">
             <p>Select Size</p>
-            <div class="product-size-select">
+            <div className="product-size-select">
               <input name="size" id="size-s" type="radio" />
-              <label for="size-s">US S</label>
+              <label htmlFor="size-s">US S</label>
               <input name="size" id="size-m" type="radio" />
-              <label for="size-m">US M</label>
+              <label htmlFor="size-m">US M</label>
               <input name="size" id="size-lg" type="radio" />
-              <label for="size-lg">US L</label>
+              <label htmlFor="size-lg">US L</label>
             </div>
-            <button class="btn outline-primary">Add to Cart</button>
+            <button className="btn outline-primary">Add to Cart</button>
           </form>
-          <div class="product-detail-content">
-            <h2 class="product-detail-header">{data.title}</h2>
-            <p class="product-detail-price">₹ {data.price}</p>
+          <div className="product-detail-content">
+            <h2 className="product-detail-header">{data.title}</h2>
+            <p className="product-detail-price">₹ {data.price}</p>
             <ul>
               <li>Everyday midweight cotton feels soft and comfortable.</li>
               <li>100% cotton</li>
--- src/pages/SingleProductPage/components/RecommendProduct/RecommendProduct.jsx
+++ src/pages/SingleProductPage/components/RecommendProduct/RecommendProduct.jsx
@@ -4,10 +4,10 @@ import { realMadrid, manchesterUnited } from '../../../../assets';
 function RecommendProduct() {
   return (
     <>
-      <section class="recommended-products">
-        <h2 class="recommended-products-header">You may also like</h2>
-        <div class="underline"></div>
-        <div class="grid grid-column-4">
+      <section className="recommended-products">
+        <h2 className="recommended-products-header">You may also like</h2>
+        <div className="underline"></div>
+        <div className="grid grid-column-4">
           <Card price={5000} imgSrc={realMadrid} name="Real Madrid Jersey" />
           <Card
             price={3000}
