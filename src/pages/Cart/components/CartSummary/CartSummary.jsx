e63f8bf9cacc47b13327bd0fd3613035c8efa9b3
Kevin-Solomon <kevinsolomon404@gmail.com>
2022-03-21 01:47:55 +0530

    refactor : add check to display for no items

--- src/pages/Cart/components/CartSummary/CartSummary.jsx
+++ src/pages/Cart/components/CartSummary/CartSummary.jsx
@@ -5,23 +5,27 @@ function CartSummary() {
   const { cartState } = useCart();
   return (
     <>
-      <div className="cart-summary">
-        <h2>Price Details</h2>
-        <div className="price">
-          <p>Price</p>
-          <p>₹ {getTotalPrice(cartState.cart)}</p>
-        </div>
+      {cartState.cart.length === 0 ? (
+        <>No items</>
+      ) : (
+        <div className="cart-summary">
+          <h2>Price Details</h2>
+          <div className="price">
+            <p>Price</p>
+            <p>₹ {getTotalPrice(cartState.cart)}</p>
+          </div>
 
-        <div className="delivery">
-          <p>Delivery Charges</p>
-          <p>₹ 499</p>
+          <div className="delivery">
+            <p>Delivery Charges</p>
+            <p>₹ 499</p>
+          </div>
+          <div className="total-amount">
+            <p>Total Amount</p>
+            <p>₹ {499 + getTotalPrice(cartState.cart)}</p>
+          </div>
+          <button className="btn primary-btn">Place Order</button>
         </div>
-        <div className="total-amount">
-          <p>Total Amount</p>
-          <p>₹ {499 + getTotalPrice(cartState.cart)}</p>
-        </div>
-        <button className="btn primary-btn">Place Order</button>
-      </div>
+      )}
     </>
   );
 }
